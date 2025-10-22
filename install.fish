92b91b7517482e87061b642ff57945eb31f5cce0
joki <mathisjoelk@outlook.de>
2025-10-22 20:22:29 +0200

    Add --noconfirm to installs

--- install.fish
+++ install.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 echo "Installing yay"
-sudo pacman -S yay
+sudo pacman -S yay --noconfirm
 
 echo "Updating the System"
 yay --noconfirm
@@ -20,7 +20,7 @@ echo "Installing caelestia and hyprland dots..."
 cp -r caelestia ~/.config/
 
 echo "Installing NeoVim"
-sudo pacman -S neovim
+sudo pacman -S neovim --noconfirm
 echo "Installing nvim dots..."
 cp -r nvim ~/.config/
 echo "Finished."

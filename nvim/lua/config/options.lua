73ad895e946de3f2ddb98027b7646617892de1b5
joki <mathisjoelk@outlook.de>
2025-11-11 22:30:53 +0100

    Nvim: add linebreaks instead of scrolling long lines

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.wrap = true -- Zeilenumbruch aktivieren
+vim.opt.linebreak = true -- nur an "sinnvollen" Stellen umbrechen (nach Leerzeichen etc.)
+vim.opt.showbreak = "↳ " -- (optional) Symbol am Zeilenumbruch anzeigen

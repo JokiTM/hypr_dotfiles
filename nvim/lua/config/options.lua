c933d196c9ed6fbd23ea2f4d0d21f4b1dee44d5f
joki <mathisjoelk@outlook.de>
2026-01-20 21:34:57 +0100

    Add binds for resizing, add // shortcut

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,11 @@
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', {silent = true})
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', {silent = true})
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', {silent = true})
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', {silent = true})
+
+vim.keymap.set('n', '<c-Left>', ':vertical-resize +5<CR>', {silent = true})
+vim.keymap.set('n', '<c-Right>', ':vertical-resize -5<CR>', {silent = true})
 
 -- vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.g.mapleader = ' '
@@ -38,3 +41,21 @@ vim.opt_local.spelllang = { "de" , "en"}
 vim.opt.title = true
 vim.opt.titlelen = 0
 vim.opt.titlestring = "nvim %t"
+
+-- search for visually selected text with //
+vim.keymap.set("v", "//", function()
+  vim.cmd("normal! y")
+  local text = vim.fn.getreg('"')
+  text = vim.fn.escape(text, [[/\]])
+  vim.fn.setreg("/", "\\V" .. text)
+  vim.cmd("normal! n")
+end, {
+  silent = true,
+  desc = "Search for visual selection",
+})
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
+  end,
+})

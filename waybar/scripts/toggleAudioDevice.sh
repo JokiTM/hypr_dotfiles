64a3c8052df49688281c353b07bbe88dd6cd5b58
joki <mathisjoelk@outlook.de>
2026-03-27 13:48:21 +0100

    cleanup waybar colors and add audio-device-toggle

---
+++ waybar/scripts/toggleAudioDevice.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curr=$(pactl get-default-sink)
+focusrite="alsa_output.usb-Focusrite_Scarlett_2i2_USB_Y8NEZ2A294B725-00.Direct__Direct__sink"
+headphones="alsa_output.pci-0000_12_00.6.analog-stereo"
+
+if [ "$curr" = $focusrite ]; then
+ pactl set-default-sink $headphones
+else
+ pactl set-default-sink $focusrite
+fi
+

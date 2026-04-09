8647253ac2dee195cfae1c923dc7727b98485f25
Mathis Kaiser <mathisjoelk@outlook.de>
2026-04-09 16:27:42 +0200

    improve zathura opening of files

--- zathura/firefoxPDF.sh
+++ zathura/firefoxPDF.sh
@@ -2,8 +2,14 @@
 
 tmpDir=/tmp/firePDF
 mkdir -p $tmpDir
-tmpFile=$(mktemp -q ${tmpDir}/firepdf_XXX.pdf)
-cd /home/mathis/Downloads/ # cd into dowloads so u can save in zathura with ':write tmpFilename.pdf'
-mv "$1" $tmpFile 
-zathura $tmpFile
-trash $tmpFile
+#tmpFile=$(mktemp -q ${tmpDir}/firepdf_XXX.pdf)
+cd /home/mathis/ # cd into home so u can save in zathura with ':write tmpFilename.pdf'
+file="$tmpDir/$(basename "$1")"
+notify-send "$file"
+if [[ -e "$file" ]]; then
+    echo "File $(basename "$1") exists"
+    exit 1
+fi
+mv "$1" "$file"
+zathura "$file"
+trash "$file"

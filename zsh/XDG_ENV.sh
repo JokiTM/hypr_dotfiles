02f99fc2b2c3fc978e7d5c120873e86eb6f6046c
Mathis Kaiser <mathisjoelk@outlook.de>
2026-04-23 14:39:05 +0200

    xdg xdg xdg sahur

--- zsh/XDG_ENV.sh
+++ zsh/XDG_ENV.sh
@@ -8,7 +8,10 @@ export XDG_DATA_HOME=$HOME/.local/share
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 
 # java
-export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=${XDG_CONFIG_HOME}/java -Djavafx.cachedir=${XDG_CACHE_HOME}/openjfx"
+
+export MAVEN_OPTS=-Dmaven.repo.local="$XDG_DATA_HOME"/maven/repository
+export MAVEN_ARGS="--settings $XDG_CONFIG_HOME/maven/settings.xml"
 
 # wget
 alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
